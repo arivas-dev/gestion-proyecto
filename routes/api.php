d70e96c5d64fa1543a1bba87fb2a5bdf469fb63a
Alexander Rivas <alexanderrivas@MacBook-Pro.local>
2026-01-16 23:44:34 -0600

    Fix: Add api prefix to route names to avoid conflicts

--- routes/api.php
+++ routes/api.php
@@ -10,9 +10,21 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(\App\Http\Middleware\AuthenticateApiToken::class)->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     
-    Route::apiResource('projects', ApiProjectController::class);
+    Route::apiResource('projects', ApiProjectController::class)->names([
+        'index' => 'api.projects.index',
+        'store' => 'api.projects.store',
+        'show' => 'api.projects.show',
+        'update' => 'api.projects.update',
+        'destroy' => 'api.projects.destroy',
+    ]);
     
     Route::prefix('projects/{project}')->group(function () {
-        Route::apiResource('tasks', ApiTaskController::class);
+        Route::apiResource('tasks', ApiTaskController::class)->names([
+            'index' => 'api.projects.tasks.index',
+            'store' => 'api.projects.tasks.store',
+            'show' => 'api.projects.tasks.show',
+            'update' => 'api.projects.tasks.update',
+            'destroy' => 'api.projects.tasks.destroy',
+        ]);
     });
 });
